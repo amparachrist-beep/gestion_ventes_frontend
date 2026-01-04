0fcf9f34fe6a226d9f274007f6d58bfd160db495
Franck242-franck <nkouampafranck49@gmail.com>
2026-01-04 23:34:21 +0100

    feat: Ajout Landing Page, MAJ Routing et Branding PWA (Hitch-Vente)

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'sales-management-v2';
+const CACHE_NAME = 'sales-management-v3';
 const urlsToCache = [
   '/',
   '/index.html',
--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,8 @@
+// main.jsx
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-
-// DÉSACTIVER LE SERVICE WORKER
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.getRegistrations().then(function(registrations) {
-    registrations.forEach(function(registration) {
-      registration.unregister();
-      console.log('Service Worker désactivé');
-    });
-  });
-}
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
